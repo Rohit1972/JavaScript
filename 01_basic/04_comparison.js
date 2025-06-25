882461a929d6d9644e3230dc30ace63992c239f6
Rohit1972 <rohitdogra1972@gmail.com>
2025-06-25 17:06:11 +0530

    Comparison

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1)
+// console.log(2 != 1);
+
+console.log("2" > 1);//true
+console.log("02" > 1); //true
+
+
+console.log(null > 0);//false
+console.log(null == 0);//false
+console.log(null >= 0);//true
+
+console.log(undefined == 0);//false
+console.log(undefined > 0);//false
+console.log(undefined < 0);//false
+
+
+// === strcitly check value and datatype
+
+console.log("2" === 2);//false
+
+
+
