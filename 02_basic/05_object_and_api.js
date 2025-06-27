47297729785afa71fce87c70a43f893e4e7537bf
Rohit1972 <rohitdogra1972@gmail.com>
2025-06-27 17:12:09 +0530

    json talk

---
+++ 02_basic/05_object_and_api.js
@@ -0,0 +1,33 @@
+// Object destructure
+const course = {
+    courseName: "Javascript",
+    price: "999",
+    cousreInstructor:"John"
+
+}
+
+
+// course.cousreInstructor
+
+const { cousreInstructor:instructor } = course   // Extracting value form object for direct use
+
+console.log(instructor);
+
+
+// APIs
+
+// {
+//     "name": "rohit",
+//     "price": "free",
+//     "courseName":"Java"
+// }
+
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
+
+
+// example : randomuserme on google
