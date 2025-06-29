de202167c198ac2124cb84590be9f847df39c1fa
Rohit1972 <rohitdogra1972@gmail.com>
2025-06-29 18:46:24 +0530

    while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,23 @@
+let index = 1
+
+while (index<=10) {
+    console.log(`Value of index is ${index}`);
+    index=index+2
+}
+
+
+
+let array = ['flash', 'batman', 'suoerman']
+let arr = 0;
+while (arr < array.length) {
+    console.log(`value is ${array[arr]}`);
+    arr=arr+1
+}
+
+
+
+let score=1
+do {
+    console.log(`Score is ${score}`);
+    score = score + 1
+} while (score <= 10);
